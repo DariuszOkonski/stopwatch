1f3cbddaf78d55f9b1b37238fcdcdbddf4a099a6
Dariusz Okoński <darek200180@gmail.com>
2025-06-12 10:59:35 +0200

    add disability button

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,16 +1,45 @@
+import { useState } from 'react';
 import Button from '../Button/Button';
 import Display from '../Display/Display';
 import styles from './App.module.scss';
 
 function App() {
+  const [time, setTime] = useState(0);
+  const [timeInterval, setTimeInterval] = useState(null);
+
+  const handleStart = () => {
+    console.log('start');
+    const timeIntervalId = setInterval(() => {
+      setTime((prevTime) => prevTime + 1);
+    }, 1000);
+    setTimeInterval(timeIntervalId);
+  };
+  const handleStop = () => {
+    console.log('stop');
+    clearInterval(timeInterval);
+    setTimeInterval(null);
+  };
+  const handleReset = () => {
+    console.log('reset');
+    clearInterval(timeInterval);
+    setTime(0);
+    setTimeInterval(null);
+  };
+
   return (
     <div className={styles.app}>
       <Display />
 
       <div className={styles.buttons}>
-        <Button>start</Button>
-        <Button>stop</Button>
-        <Button>reset</Button>
+        <Button disabled={false} action={handleStart}>
+          start
+        </Button>
+        <Button disabled={false} action={handleStop}>
+          stop
+        </Button>
+        <Button disabled={true} action={handleReset}>
+          reset
+        </Button>
       </div>
     </div>
   );
--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,7 +1,15 @@
 import styles from './Button.module.scss';
 
-const Button = ({ children }) => {
-  return <button className={styles.button}>{children}</button>;
+const Button = ({ children, action, disabled }) => {
+  return (
+    <button
+      onClick={action}
+      className={`${styles.button} ${disabled ? styles.disabled : ''}`}
+      disabled={disabled}
+    >
+      {children}
+    </button>
+  );
 };
 
 export default Button;
